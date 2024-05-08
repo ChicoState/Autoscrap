612ad4665f23984ca32b7489831fe606a78fe201
csucrljohnson1 <rljohnson1@csuchico.edu>
2024-05-08 13:39:14 -0700

    added more userManager Tests

--- spec/user.spec.js
+++ spec/user.spec.js
@@ -30,4 +30,21 @@ describe('addUser', () => {
         expect(user.password).toBe('testPass');
         addUserStub.restore();
     });
+    it('should pass in testName and testPass into db and expect the titles not to be swapped', async() => {
+        const addUserStub = sinon.stub(userManager, 'addUser');
+        addUserStub.withArgs('testName', 'testPass').resolves({username: 'testName', password: 'testPass'});
+        const user = await userManager.addUser('testName', 'testPass');
+        expect(user.username).not.toBe('testPass');
+        expect(user.password).not.toBe('testName');
+        addUserStub.restore();
+    });
+    it('should pass in data into db and not be empty', async() => { //potential bug if allowed
+        const addUserStub = sinon.stub(userManager, 'addUser');
+        addUserStub.withArgs('', '').resolves({username: '', password: ''});
+        const user = await userManager.addUser('', '');
+        expect(user.username).toBe('');
+        expect(user.password).toBe('');
+        addUserStub.restore();
+    });
 });
+
